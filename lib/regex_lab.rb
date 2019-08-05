8bdfe28af3eb17c22d47032f2ec3617e7997ddb9
Micah Levine <micah.levine@outlook.com>
2019-08-05 19:13:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
+ if word.match (/^[aeiouAEIOU]/)
+  true
+else
+  false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(/^[A-Z].*[\.,:;]$/)
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match (/.?\d{3}.?\d{3}.?\d{4}\b/)
+  true
+else
+  false
+  end
 end
